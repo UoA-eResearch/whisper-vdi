f9f2e17ad405f732365081e9f4265cc296c4f3da
NidhiGowdra <nidhign11@gmail.com>
2025-02-11 22:11:04 +0000

    Feat: added jiwer CeR for comparision

--- src/cer.py
+++ src/cer.py
@@ -1,7 +1,9 @@
 from torchmetrics.text import CharErrorRate
 #preds = ['Aunty, tēnā koe, i whakapūare nei i ngā kuaha o tēnei whare i te rā nei,']
 #target = ['Aunty, tēnā koe, i whakapūare nei i ngā kuaha o tēnei whare i te rā nei,']
-cer = CharErrorRate()
+torch_cer = CharErrorRate()
+
+from jiwer import wer, cer
 
 preds = []
 target = []
@@ -14,9 +16,11 @@ gt = str(gt).lower()
 preds = open(path + model_name + '.txt', "rt")
 preds = str(preds).lower()
 
-cer = CharErrorRate()
-res = cer(preds, gt)
+res = torch_cer(preds, gt)
 print(str(res.numpy()))
+
+error = cer(gt, preds)
+print(error)
 
 with open(path + 'transcription_times' + '.csv', 'a') as f:
     f.write(str(res.numpy()) + '\n')
