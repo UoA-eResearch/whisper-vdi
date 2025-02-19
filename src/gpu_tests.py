67b90aa4bdfe591260409801d48a2c12692ecae4
Nick Young <nick.young@auckland.ac.nz>
2025-02-19 13:18:19 +1300

    run GPU tests across multiple different files

--- src/gpu_tests.py
+++ src/gpu_tests.py
@@ -2,33 +2,47 @@
 
 import requests
 import pandas as pd
+from glob import glob
+import librosa
+from tqdm.auto import tqdm
+import os
 
+df = pd.DataFrame({"filename": glob("data/*.mp3")})
+df["duration"] = df["filename"].apply(lambda f: librosa.get_duration(path=f))
+df.drop_duplicates("duration", inplace=True)
+print(df)
 results = []
 
-def save(hypothesis, name=""):
-  with open(f"data/{name}.txt", 'w') as f:
-      f.write(hypothesis)
+for row in tqdm(df.itertuples(index=False), total=len(df)):
+    # A100 + insanely-fast-whisper + large-v3 (Transformers) (fp16 + batching [24] + Flash Attention 2)
+    r = requests.post("https://a100.auckland-cer.cloud.edu.au/upload", files={"audio": open(row.filename, "rb").read()})#, data={"language": "mi"})
+    hypothesis = r.json()["output"]["text"]
+    name = "A100 + insanely-fast-whisper + large-v3 (Transformers) (fp16 + batching [24] + Flash Attention 2)"
+    results.append({
+        "name": name,
+        "file": os.path.basename(row.filename),
+        "file_duration": row.duration,
+        "time": r.elapsed.total_seconds(),
+        "time_per_second": r.elapsed.total_seconds() / row.duration,
+        "transcript": hypothesis
+    })
 
-# A100 + insanely-fast-whisper + large-v3 (Transformers) (fp16 + batching [24] + Flash Attention 2)
-r = requests.post("https://a100.auckland-cer.cloud.edu.au/upload", files={"audio": open("data/094337_tagged_20170915759438_paraini.mp3", "rb").read()})#, data={"language": "mi"})
-hypothesis = r.json()["output"]["text"]
-name = "A100 + insanely-fast-whisper + large-v3 (Transformers) (fp16 + batching [24] + Flash Attention 2)"
-save(hypothesis, name)
-results.append({
-    "name": name,
-    "time": r.elapsed.total_seconds()
-})
+print(pd.DataFrame(results))
+pd.DataFrame(results).to_csv("gpu_tests.csv", index=False)
 
-# A100 + faster-whisper + large-v3
-r = requests.post("https://asr.auckland-cer.cloud.edu.au/asr", files={"audio_file": open("data/094337_tagged_20170915759438_paraini.mp3", "rb").read()})#, params={"language": "mi"})
-hypothesis = r.text.replace("\n", " ")
-name = "A100 + faster-whisper + large-v3"
-metrics = save(hypothesis, name)
-results.append({
-    "name": name,
-    "time": r.elapsed.total_seconds()
-})
+for row in tqdm(df.itertuples(index=False), total=len(df)):
+    # A100 + faster-whisper + large-v3
+    r = requests.post("https://asr.auckland-cer.cloud.edu.au/asr", files={"audio_file": open(row.filename, "rb").read()})#, params={"language": "mi"})
+    hypothesis = r.text.replace("\n", " ")
+    name = "A100 + faster-whisper + large-v3"
+    results.append({
+        "name": name,
+        "file": os.path.basename(row.filename),
+        "file_duration": row.duration,
+        "time": r.elapsed.total_seconds(),
+        "time_per_second": r.elapsed.total_seconds() / row.duration,
+        "transcript": hypothesis
+    })
 
-df = pd.DataFrame(results)
-print(df)
-df.to_csv("gpu_tests.csv", index=False)
+print(pd.DataFrame(results))
+pd.DataFrame(results).to_csv("gpu_tests.csv", index=False)
